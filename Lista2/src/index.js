9716cd848ff11f623d16fa0938511160b59aea11
isabellyandrade <isabellyandradep@gmail.com>
2024-03-08 21:16:16 -0300

    Terminei a lista 2

--- Lista2/src/index.js
+++ Lista2/src/index.js
@@ -1,4 +1,5 @@
 "use strict";
+//Exercicio 1
 class Carro {
     constructor(marca, modelo, anoLançamento) {
         this.marca = marca;
@@ -22,3 +23,92 @@ class Carro {
 let carro = new Carro("Fiat", "Argo", 2022);
 console.log("\n1)");
 console.log(carro.idadeCarro());
+//Exercicio 2
+class Calculadora {
+    constructor(valor1, valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+    soma() {
+        let soma = this.valor1 + this.valor2;
+        return "Resultado da soma é: " + soma.toString();
+    }
+    sub() {
+        let sub = this.valor1 - this.valor2;
+        return "Resultado da subtração é: " + sub.toString();
+    }
+    mult() {
+        let mult = this.valor1 * this.valor2;
+        return "Resultado da multiplicação é: " + mult.toString();
+    }
+    div() {
+        let div = this.valor1 / this.valor2;
+        return "Resultado da divisão é: " + div.toString();
+    }
+    porcent() {
+        let porcent = (this.valor1 * this.valor2) / 100;
+        return "Resultado da porcentagem é: " + porcent.toString();
+    }
+    set setValor1(valor1) {
+        this.valor1 = valor1;
+    }
+    get getValor1() {
+        return this.valor1;
+    }
+    get getValor2() {
+        return this.valor2;
+    }
+}
+let calculadora = new Calculadora(0, 50);
+console.log("\n2)");
+console.log(calculadora.soma());
+console.log(calculadora.sub());
+console.log(calculadora.mult());
+console.log(calculadora.div());
+console.log(calculadora.porcent());
+calculadora.setValor1 = 2;
+console.log(calculadora.div());
+//Exercicio 3
+class Produto {
+    constructor(nome, preço, quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+    calcularValorTotalEmEstoque() {
+        let total = this.preço * this.quantidadeEmEstoque;
+        return "O total é: " + total.toString();
+    }
+    reporEstoque(quantidade) {
+        let i;
+        this.quantidadeEmEstoque += quantidade;
+        return "Estoque adicionado com sucesso! Estoque atual: " + this.quantidadeEmEstoque;
+    }
+    vender(quantidade) {
+        if (this.quantidadeEmEstoque < quantidade) {
+            return "Estoque insuficiente";
+        }
+        else {
+            this.quantidadeEmEstoque -= quantidade;
+            return "Removido com sucesso! Estoque atual: " + this.quantidadeEmEstoque;
+        }
+    }
+    get getNome() {
+        return this.nome;
+    }
+    get getPreço() {
+        return this.preço;
+    }
+    get getQuantidadeEmEstoque() {
+        return this.quantidadeEmEstoque;
+    }
+    set setQuantidadeEmEstoque(quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
+let produto = new Produto("Rimel", 20, 10);
+console.log("\n3)");
+console.log(produto.calcularValorTotalEmEstoque());
+console.log(produto.reporEstoque(10));
+console.log(produto.vender(2));
+console.log(produto.calcularValorTotalEmEstoque());
--- Lista2/src/index.ts
+++ Lista2/src/index.ts
@@ -1,3 +1,4 @@
+//Exercicio 1
 class Carro{
     private marca: string;
     private modelo: string;
@@ -31,3 +32,127 @@ let carro = new Carro("Fiat", "Argo", 2022);
 
 console.log("\n1)")
 console.log(carro.idadeCarro());
+
+
+//Exercicio 2
+
+class Calculadora
+{
+    private valor1: number;
+    private valor2: number;
+
+    constructor(valor1: number, valor2:number){
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    soma():string{
+        let soma:number = this.valor1 + this.valor2;
+
+        return "Resultado da soma é: " + soma.toString();
+    }
+
+    sub():string{
+        let sub:number = this.valor1 - this.valor2;
+
+        return "Resultado da subtração é: " + sub.toString();
+    }
+    mult():string{
+        let mult:number = this.valor1 * this.valor2;
+
+        return "Resultado da multiplicação é: " + mult.toString();
+    }
+    div():string{
+        let div:number = this.valor1 / this.valor2;
+         
+        return "Resultado da divisão é: " + div.toString();
+    
+    }
+    porcent():string{
+        let porcent:number = (this.valor1 * this.valor2) / 100;
+
+        return "Resultado da porcentagem é: " + porcent.toString();
+    }
+    set setValor1(valor1:number){
+        this.valor1 = valor1;
+    }
+    get getValor1():number{
+        return this.valor1
+    }
+    get getValor2():number{
+        return this.valor2
+    }
+
+}
+
+
+let calculadora = new Calculadora(0, 50);
+
+console.log("\n2)")
+console.log(calculadora.soma());
+console.log(calculadora.sub());
+console.log(calculadora.mult());
+console.log(calculadora.div());
+console.log(calculadora.porcent());
+calculadora.setValor1 = 2;
+console.log(calculadora.div());
+
+
+//Exercicio 3
+
+class Produto
+{
+    nome:string;
+    preço:number;
+    quantidadeEmEstoque: number;
+
+    constructor(nome: string, preço:number, quantidadeEmEstoque:number){
+        this.nome = nome;
+        this.preço = preço;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    calcularValorTotalEmEstoque(){
+        let total:number = this.preço * this.quantidadeEmEstoque;
+
+        return "O total é: " + total.toString();
+    }
+
+    reporEstoque(quantidade: number){
+        let i;
+
+            this.quantidadeEmEstoque += quantidade;
+
+        return "Estoque adicionado com sucesso! Estoque atual: " + this.quantidadeEmEstoque;
+    }
+    vender(quantidade: number){
+        
+        if(this.quantidadeEmEstoque < quantidade){
+            return "Estoque insuficiente";
+        } else {
+            this.quantidadeEmEstoque -= quantidade;
+            return "Removido com sucesso! Estoque atual: " + this.quantidadeEmEstoque;
+        }
+    }
+    get getNome():string{
+        return this.nome;
+    }
+    
+    get getPreço():number{
+        return this.preço;
+    }
+    get getQuantidadeEmEstoque():number{
+        return this.quantidadeEmEstoque;
+    }
+    set setQuantidadeEmEstoque(quantidadeEmEstoque:number){
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
+
+let produto = new Produto("Rimel", 20, 10);
+
+console.log("\n3)");
+console.log(produto.calcularValorTotalEmEstoque());
+console.log(produto.reporEstoque(10));
+console.log(produto.vender(2));
+console.log(produto.calcularValorTotalEmEstoque());
